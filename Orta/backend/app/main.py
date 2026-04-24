bd590e8e28bef8af738dff46e755a2788011b309
Diospanov <diasmegaboss@gmail.com>
2026-04-24 22:38:39 +0500

    add vercel url to cors

--- Orta/backend/app/main.py
+++ Orta/backend/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://orta-tau.vercel.app",
+        "https://orta-tau.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -25,3 +25,9 @@ app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(teams_router)
 app.include_router(join_request_router)
+
+print("CORS READY FOR:", [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://orta-tau.vercel.app",
+])
