14322cae17f0a4e5825e32caaa3909c79245ae8a
Diospanov <diasmegaboss@gmail.com>
2026-04-24 22:56:16 +0500

    test open cors

--- Orta/backend/app/main.py
+++ Orta/backend/app/main.py
@@ -11,12 +11,8 @@ app = FastAPI(title="Orta API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://orta-tau.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
